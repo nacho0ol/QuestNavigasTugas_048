8bacb456e6ad12eb5b3d7d3c2dc4108931a2597b
nachool <nachma.olivia1394@gmail.com>
2025-11-09 13:45:06 +0700

    menambah data

--- app/src/main/java/com/example/questnavigastugas_048/data/DataSource.kt
+++ app/src/main/java/com/example/questnavigastugas_048/data/DataSource.kt
@@ -8,3 +8,44 @@ data class Peserta(
     val status: String,
     val alamat: String
 )
+
+object DataSource {
+    val listPeserta = listOf(
+        Peserta(
+            nama = "Kairi Kumar",
+            gender = "Laki-laki",
+            status = "Cerai",
+            alamat = "jl pegangsaan timur"
+        ),
+        Peserta(
+            nama = "badut petrik",
+            gender = "Perempuan",
+            status = "Lajang",
+            alamat = "sleman"
+        ),
+        Peserta(
+            nama = "pitekantropus",
+            gender = "laki-laki",
+            status = "Kawin",
+            alamat = "sangiran"
+        ),
+        Peserta(
+            nama = "imroatus",
+            gender = "Laki-laki",
+            status = "Lajang",
+            alamat = "parkiran UB"
+        ),
+        Peserta(
+            nama = "sabo",
+            gender = "Perempuan",
+            status = "Lajang",
+            alamat = "jl asbunawan"
+        ),
+        Peserta(
+            nama = "umi rosdiah",
+            gender = "Perempuan",
+            status = "Kawin",
+            alamat = "pochinki"
+        )
+    )
+}
