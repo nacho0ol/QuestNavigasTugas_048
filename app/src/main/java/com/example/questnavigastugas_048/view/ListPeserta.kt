d698d3ca81258cafafec5b45814f4fbce7af6585
nachool <nachma.olivia1394@gmail.com>
2025-11-09 14:37:35 +0700

    menambah row & button untuk tempat dua button beranda & formulir

--- app/src/main/java/com/example/questnavigastugas_048/view/ListPeserta.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/ListPeserta.kt
@@ -1,12 +1,16 @@
 package com.example.questnavigastugas_048.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -50,6 +54,16 @@ fun ListPeserta(
 
         Spacer(modifier = Modifier.weight(1f))
 
-
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = onBerandaClick) {
+                Text(text = stringResource(id = R.string.beranda))
+            }
+            Button(onClick = onFormulirClick) {
+                Text(text = stringResource(R.string.formulir))
+            }
+        }
     }
 }
