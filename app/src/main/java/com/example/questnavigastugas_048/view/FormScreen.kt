752d44eaa84c2fd12ab44ca1f10f3126c446d32a
nachool <nachma.olivia1394@gmail.com>
2025-11-09 14:44:50 +0700

    menambah function formscreen dan parameternya

--- app/src/main/java/com/example/questnavigastugas_048/view/FormScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/FormScreen.kt
@@ -1,2 +1,13 @@
 package com.example.questnavigastugas_048.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormScreen(
+    onKembaliClick: () -> Unit,
+    onDialogDismiss: () -> Unit,
+    modifier: Modifier = Modifier
+)
