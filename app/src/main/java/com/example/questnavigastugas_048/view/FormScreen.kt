cb69745691b870faca0971289f5ec9ddbd347651
nachool <nachma.olivia1394@gmail.com>
2025-11-09 14:54:55 +0700

    menambah text form pendaftaran

--- app/src/main/java/com/example/questnavigastugas_048/view/FormScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/FormScreen.kt
@@ -1,6 +1,12 @@
 package com.example.questnavigastugas_048.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -11,8 +17,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.questnavigastugas_048.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -58,6 +66,11 @@ fun FormScreen(
                 .padding(16.dp)
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
-        )
+        ) {
+            Text(
+                text = stringResource(R.string.form_pendaftaran),
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
     }
 }
