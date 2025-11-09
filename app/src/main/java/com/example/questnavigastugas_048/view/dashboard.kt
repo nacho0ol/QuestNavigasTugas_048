1665b2dd567a16ef6aa5b4a4da1a3d646099e93f
nachool <nachma.olivia1394@gmail.com>
2025-11-09 14:02:50 +0700

    menambah logoo

--- app/src/main/java/com/example/questnavigastugas_048/view/dashboard.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/dashboard.kt
@@ -1,16 +1,19 @@
 package com.example.questnavigastugas_048.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.questnavigastugas_048.R
@@ -32,5 +35,11 @@ fun dashboard(
             style = MaterialTheme.typography.headlineMedium
         )
         Spacer(modifier = Modifier.height(height = 32.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
     }
 }
