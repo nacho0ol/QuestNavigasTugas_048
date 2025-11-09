ea709fb2fe606e4317bd0ebbb15dda66fa81d7e4
nachool <nachma.olivia1394@gmail.com>
2025-11-09 13:58:10 +0700

    menambah parameter function

--- app/src/main/java/com/example/questnavigastugas_048/view/dashboard.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/dashboard.kt
@@ -1,2 +1,12 @@
 package com.example.questnavigastugas_048.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun dashboard(
+    onMasukClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
