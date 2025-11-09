136cc9122c09d0aa5ba7b12918e47c936a37967f
nachool <nachma.olivia1394@gmail.com>
2025-11-09 14:00:50 +0700

    menambah text selamat datang

--- app/src/main/java/com/example/questnavigastugas_048/view/dashboard.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/dashboard.kt
@@ -4,10 +4,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.questnavigastugas_048.R
 
 @Composable
 fun dashboard(
@@ -21,6 +25,9 @@ fun dashboard(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
-
+        Text(
+            text = stringResource(id = R.string.selamat_datang),
+            style = MaterialTheme.typography.headlineMedium
+        )
     }
 }
