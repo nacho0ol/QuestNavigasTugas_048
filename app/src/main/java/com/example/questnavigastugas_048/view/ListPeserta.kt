4f9a91ccc18430dfb92736c61ab17321114bdde1
nachool <nachma.olivia1394@gmail.com>
2025-11-09 14:24:14 +0700

    menambah text list_peserta

--- app/src/main/java/com/example/questnavigastugas_048/view/ListPeserta.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/ListPeserta.kt
@@ -5,10 +5,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.questnavigastugas_048.R
 import com.example.questnavigastugas_048.data.DataSource
 
 @Composable
@@ -26,6 +30,9 @@ fun ListPeserta(
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-
+        Text(
+            text = stringResource(id = R.string.list_peserta),
+            style = MaterialTheme.typography.headlineMedium
+        )
     }
 }
