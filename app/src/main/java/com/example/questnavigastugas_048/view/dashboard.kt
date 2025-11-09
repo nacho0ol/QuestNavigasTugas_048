c1eed56cdf23b41085623ce244cee4d2f9068ea9
nachool <nachma.olivia1394@gmail.com>
2025-11-09 13:59:31 +0700

    menambah column untuk root nya

--- app/src/main/java/com/example/questnavigastugas_048/view/dashboard.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/dashboard.kt
@@ -1,12 +1,26 @@
 package com.example.questnavigastugas_048.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun dashboard(
     onMasukClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
 
+    }
 }
