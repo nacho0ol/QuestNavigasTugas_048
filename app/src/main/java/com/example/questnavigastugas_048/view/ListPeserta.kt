fa8177c647f63bc65c27597d3fb4667a37ffa384
nachool <nachma.olivia1394@gmail.com>
2025-11-09 14:23:24 +0700

    menambah column

--- app/src/main/java/com/example/questnavigastugas_048/view/ListPeserta.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/ListPeserta.kt
@@ -1,7 +1,14 @@
 package com.example.questnavigastugas_048.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.questnavigastugas_048.data.DataSource
 
 @Composable
@@ -12,4 +19,13 @@ fun ListPeserta(
     modifier: Modifier = Modifier
 ){
     val listPeserta = DataSource.listPeserta
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
 }
