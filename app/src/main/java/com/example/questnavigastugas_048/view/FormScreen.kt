e649596592324b40ef3f864ddded18265826b920
nachool <nachma.olivia1394@gmail.com>
2025-11-09 14:54:06 +0700

    menambah column di innerpadding

--- app/src/main/java/com/example/questnavigastugas_048/view/FormScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/FormScreen.kt
@@ -1,12 +1,19 @@
 package com.example.questnavigastugas_048.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigastugas_048.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,6 +50,14 @@ fun FormScreen(
             )
         }
     ) {
-
+        innerPadding ->
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(innerPadding)
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
     }
 }
