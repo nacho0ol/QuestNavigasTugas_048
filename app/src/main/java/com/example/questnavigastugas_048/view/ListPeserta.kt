5a4d5dfa72b66c97fa4e455cbaf4a77c511f5517
nachool <nachma.olivia1394@gmail.com>
2025-11-09 14:20:51 +0700

    menambah function ListPeserta dan menambah parameter

--- app/src/main/java/com/example/questnavigastugas_048/view/ListPeserta.kt
+++ app/src/main/java/com/example/questnavigastugas_048/view/ListPeserta.kt
@@ -1,2 +1,14 @@
 package com.example.questnavigastugas_048.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ListPeserta(
+    isLoading: Boolean,
+    onBerandaClick: () -> Unit,
+    onFormulirClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
