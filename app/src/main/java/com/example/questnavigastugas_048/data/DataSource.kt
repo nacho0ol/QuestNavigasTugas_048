503b3d29ad5452d8aecf4a60f68bb35ad074be91
nachool <nachma.olivia1394@gmail.com>
2025-11-09 13:29:12 +0700

    menambah variabel tetap (val) pada class peserta untuk data peserta

--- app/src/main/java/com/example/questnavigastugas_048/data/DataSource.kt
+++ app/src/main/java/com/example/questnavigastugas_048/data/DataSource.kt
@@ -1,2 +1,10 @@
 package com.example.questnavigastugas_048.data
 
+import android.R
+
+data class Peserta(
+    val nama: String,
+    val gender: String,
+    val status: String,
+    val alamat: String
+)
